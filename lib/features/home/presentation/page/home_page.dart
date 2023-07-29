4f13869283b2e38f97cc332fdb0a24f00dfab7eb
YOUSEF <yousef.habbeh@mail.ru>
2023-07-29 15:14:57 +0300

    create home feature

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Row(children: [
         Column(
           children: [
