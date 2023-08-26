42b9370fe25552795a3c240be841b2db15290951
YOUSEF <yousef.habbeh@mail.ru>
2023-08-27 01:00:40 +0300

    create new branch from last work

--- lib/core/resources/constants.dart
+++ lib/core/resources/constants.dart
@@ -1,6 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 
-const String BASE_URL = 'http://yas0of.pythonanywhere.com/';
+const String BASE_URL = 'http://yas0of.pythonanywhere.com';
 const String LOGIN = '/login';
 String assetsAudioPath = 'nature.mp3';
 AudioPlayer audioPlayer = AudioPlayer();
